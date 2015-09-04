3b55cd62b1b711be605bc66a1269c4d891f40928
agGreece <agravarismeden@gmail.com>
2015-09-04 17:56:39 +0300

    initial commit

---
+++ src/playersData.java
@@ -0,0 +1,15 @@
+
+public class playersData {
+	
+	String name;
+	
+	public void setName(String n){
+		name = n;
+	}
+	
+	public void getName(String n){
+		return name;  
+				
+	}
+
+}
---
+++ src/testRock_1.java
@@ -0,0 +1,17 @@
+
+public class testRock_1 {
+
+	import java.util.Scanner;
+	
+	public static void main(String[] args) {
+		
+		Scanner myScan= new Scanner (System.in);
+		
+		playersData player1 = new playersData();
+		
+		System.out.println("please enter your name");
+        String player1Name = playerScan.nextLine();	 
+		player1.setName(player1Name);
+	}
+
+
